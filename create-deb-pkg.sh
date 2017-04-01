d5ccf6dc7c043cb914070b84ac5ebd7c1f55bb13
Cerem Cem ASLAN <cem@aktos.io>
2017-04-02 00:36:08 +0300

    improved create-deb-pkg.sh

--- create-deb-pkg.sh
+++ create-deb-pkg.sh
@@ -5,11 +5,26 @@ DIR=$(dirname "$(readlink -f "$0")")
 source $DIR/conf.sh
 DATE=$(date +"%Y%d%m")
 cd $BUILD_DIR
+
+PACKAGE_NAME="kicad-$DATE-$(uname -m)"
+
+echo "Creating $PACKAGE_NAME"
+
+START_TIME=$SECONDS
 sudo cpack \
-    -D CPACK_SYSTEM_NAME="kicad-$DATE-$(uname -m)" \
+    -D CPACK_PACKAGE_FILE_NAME="$PACKAGE_NAME" \
     -D CPACK_PACKAGE_NAME="kicad" \
     -D CPACK_PACKAGE_VERSION="$DATE" \
     -D CPACK_PACKAGE_VERSION_MAJOR="$DATE" \
     -D CPACK_PACKAGE_VERSION_MINOR="0" \
     -D CPACK_PACKAGE_VERSION_PATCH="0"
 
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
+echo 
+echo "(process took $ELAPSED_TIME seconds...)"
+echo 
+echo "You can install generated package by issuing the following command: "
+echo
+echo "    sudo dpkg -i $BUILD_DIR/$PACKAGE_NAME.deb"
+echo
